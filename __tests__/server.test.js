8b5f4bb1a74a0f561a22d9fb4f20d2319876b9a0
nacerillo <nicholascerillo@gmail.com>
2021-04-22 19:38:56 -0700

    updating dev

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,9 +1,9 @@
 "user strict";
 
 const { server } = require("../src/server.js");
-const supertest = require("supertest"); // mock request engine
-const mockRequest = supertest(server);
-require("@code-fellows/supergoose"); // this pulls in and configures and runs mongo memory server and supertest
+const supergoose = require("@code-fellows/supergoose"); // mock request engine
+const mockRequest = supergoose(server);
+// this pulls in and configures and runs mongo memory server and supertest
 
 const DataCollection = require("../src/models/data-collection-class.js");
 //const food = new GenericCollection();
@@ -26,18 +26,13 @@ describe("Food Actions", () => {
   // create, then read so that this test is independent from above
   it("can read() a single food item", () => {
     let obj = { name: "test food 2", calories: 9999, type: "VEG" };
-    let expected = { name: "test food 1", calories: 9999, type: "FRUIT" };
+    let expected = { name: "test food 2", calories: 9999, type: "VEG" };
     return food.create(obj).then((record) => {
       return food.read(record._id).then((item) => {
-        expect(record[item]).toEqual(expected[item]);
+        console.log("this should be test food 2", item);
       });
     });
   });
-
-  it("can update a single food item", () => {
-    let obj = {};
-  });
-
   // bad path
   it("should send response 404 when bad path is given", async () => {
     return mockRequest.get("/not-a-thing").then((data) => {
@@ -48,6 +43,14 @@ describe("Food Actions", () => {
   it("should send response 404 when bad method is given", async () => {
     return mockRequest.get("/").then((data) => {
       expect(data.status).toBe(404);
+    });
+  });
+
+  it("need to be able to upate an item", async () => {
+    let obj = { name: "test food 2", calories: 9999, type: "VEG" };
+    let updated = { name: "test food 2", calories: 3000, type: "FRUIT" };
+    return food.create(obj).then((record) => {
+      return food.update(record._id, updated).then((item) => {});
     });
   });
 });
